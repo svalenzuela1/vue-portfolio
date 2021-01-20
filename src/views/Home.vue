57c0776519e2cdc4f7b96a68203ed207d1633b27
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-01-20 02:21:41 -0500

    about me desktop responsive in progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,8 +16,10 @@
     <div class="about">
 
       <img class="portfolioimg" src="https://res.cloudinary.com/stephaniev/image/upload/v1602111407/IMG_2375_sv00u0.jpg" alt="image">
-      <h3 id="aboutMe">Hi, my name is </h3>
-      <h1 class="name"> STEPHANIE VALENZUELA</h1>
+
+      <div id="fieldset1">
+        <h3 id="aboutMe">Hi, my name is </h3>
+        <h1 class="name"> STEPHANIE VALENZUELA</h1>
       <fieldset class="fieldset1"> <legend> WHO AM I?</legend>
         <p> A Junior Developer who is driven to make an impact in the tech community.
           As a child of immigrants and a first generation College graduate, I joined
@@ -25,6 +27,7 @@
           logical thinking and problem-solving, constantly in pursuit of gaining more
           knowledge.</p>
       </fieldset>
+      </div>
     </div>
   </div>
 </template>
@@ -67,7 +70,15 @@ template{
   margin: 0px auto;
   background-color: black;
 }
-/*.fieldset1 {*/
-/*  width: 100px;*/
-/*}*/
+
+
+#fieldset1{
+  padding-left: 300px;
+  padding-right: 300px;
+}
+
+.portfolioimg{
+  height: 30%;
+  width: 30%;
+}
 </style>
