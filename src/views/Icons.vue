9598ac2a499a39bac891105e7c204a77caf077df
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-01-20 00:48:44 -0500

    temporarily adding icons to Home.vue

--- src/views/Icons.vue
+++ src/views/Icons.vue
@@ -1,26 +1,19 @@
 <template>
-  <div class="home">
-<!--    <img alt="Vue logo" src="../assets/logo.png">-->
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <div class="icons">
+  <div class="icon">
     <a target="_blank" href="https://github.com/svalenzuela1">
       <img class="logo" src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png">
     </a>
     <a target="_blank" href="https://www.linkedin.com/in/stephanie-val/">
       <img class="logo" src="https://cdn.iconscout.com/icon/free/png-256/linkedin-2662666-2213265.png">
     </a>
-    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-
-  }
+  name: 'Icons',
 }
 </script>
+
+<style>
+</style>
