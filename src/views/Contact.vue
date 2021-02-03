7d8c0d8e2d2daa3751c85c32b9157a51ed182bfb
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-03 09:09:56 -0500

    version before deployment

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="contact-sheet">
   <section>
-    <form class="contact-form" @submit.prevent="sendEmail">
-    <b-field horizontal label="Subject" type="is-danger" message="Please enter a subject">
-      <b-input name="subject" expanded></b-input>
-    </b-field>
+    <form action="https://docs.google.com/forms/d/e/1FAIpQLSciTpkSCmvIMNliYziyj1vPooUO7Ixjk3IE0PBJ67_EgErdMg/viewform" method="post">
+      <label>Name*</label>
+      <input type="text" placeholder="Name*" name="Name" required>
 
-    <b-field horizontal label="From">
-      <b-input name="name" placeholder="Name" expanded></b-input>
-      <b-input name="email" type="email" placeholder="nobody@nowhere.com" expanded></b-input>
-    </b-field>
+      <label>Email Address*</label>
+      <input type="email" placeholder="Email address*" name="Email" required>
 
-    <b-field horizontal label="Message">
-      <b-input type="textarea"></b-input>
-    </b-field>
+      <label>Subject</label>
+      <input type="text" placeholder="Subject" name="Subject" required>
 
-    <b-field horizontal><!-- Label left empty for spacing -->
-      <p class="control">
-        <b-button v-on:click="sendEmail" label="Send message" type="is-primary" value="send" />
-      </p>
-    </b-field>
+      <label>Message*</label>
+      <textarea rows="5" placeholder="Message*" name="Message" required></textarea>
+
+      <button type="submit">Send</button>
     </form>
   </section>
   </div>
