98018179a8568af5fe386e8690d14b8a6036ab27
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-03 09:44:58 -0500

    trying to change form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,25 +1,15 @@
 <template>
     <div class="contact-sheet">
       <section>
-        <form name="contact" method="POST" netlify>
+        <form name="contact" netlify>
           <p>
-            <label>Your Name: <input type="text" name="name" /></label>
+            <label>Name <input type="text" name="name" /></label>
           </p>
           <p>
-            <label>Your Email: <input type="email" name="email" /></label>
+            <label>Email <input type="email" name="email" /></label>
           </p>
           <p>
-            <label>Your Role: <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-<!--            <button type="submit">Send</button>-->
-            <input type="submit">
+            <button type="submit">Send</button>
           </p>
         </form>
       </section>
