d3b7da81a8ec0fc4852e06ada993a4a4901d9edd
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-03 10:04:39 -0500

    netlify vue suggestion

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,10 @@
 <template>
     <div class="contact-sheet">
       <section>
-        <form name="contact" netlify>
-          <p>
-            <label>Name <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Email <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+          <input type="text" name="name" />
+          <input type="email" name="email" />
+          <textarea name="message"></textarea>
         </form>
       </section>
     </div>
