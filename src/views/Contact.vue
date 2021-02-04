2269b954ff71ae1524b53a9885f065df9eb1f6ed
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-04 04:16:55 -0500

    form succesfully resetted after submission

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,32 +1,19 @@
 <template>
-<!--  <div class="container">-->
-<!--      <form class="contact-form" @submit.prevent="sendEmail">-->
-<!--        <label>Name</label>-->
-<!--        <input type="text" name="user_name">-->
-<!--        <label>Email</label>-->
-<!--        <input type="email" name="user_email">-->
-<!--        <label>Message</label>-->
-<!--        <textarea name="message"></textarea>-->
-
-
-<!--        <input type="submit" value="Send" v-on:click="sendEmail">-->
-<!--      </form>-->
-<!--  </div>-->
 
   <div class="contact-sheet">
     <section>
       <form class="contact-form" @submit.prevent="sendEmail">
     <b-field horizontal label="Subject" type="is-danger" message="Please enter a subject">
-      <b-input name="subject" expanded></b-input>
+      <b-input v-model="subject" name="subject" expanded></b-input>
     </b-field>
 
     <b-field horizontal label="From">
-      <b-input name="user_name" placeholder="Name" expanded></b-input>
-      <b-input name="user_email" type="email" placeholder="nobody@nowhere.com" expanded></b-input>
+      <b-input v-model="name" name="user_name" placeholder="Name" expanded></b-input>
+      <b-input v-model="email" name="user_email" type="email" placeholder="nobody@nowhere.com" expanded></b-input>
     </b-field>
 
     <b-field horizontal label="Message">
-      <b-input type="textarea" name="message"></b-input>
+      <b-input v-model="message" type="textarea" name="message"></b-input>
     </b-field>
 
     <b-field horizontal><!-- Label left empty for spacing -->
@@ -37,6 +24,7 @@
         </form>
     </section>
   </div>
+
     </template>
 
 <script>
@@ -44,11 +32,24 @@ import emailjs from 'emailjs-com';
 
 export default {
   name: 'Contact',
+  data: function(){
+    return{
+      subject: "",
+      name: "",
+      email: "",
+      message: ""
+
+    }
+  },
   methods: {
+    redirect: function(){
+      return this.$router.push("/thanks")
+    },
     sendEmail: (e) => {
       emailjs.sendForm('service_u1jml2x', 'template_hctlnqq', e.target, 'user_OdKcH5LzTxMskLqSISB0r')
-
-      alert('Thank you for your email!')
+      alert('Thank you for your email! I will get back to you as soon as possible.')
+      e.target.reset() //will reset the form
+      this.redirect() //an attempt to display thank you page
 
     }
   }
@@ -63,50 +64,4 @@ export default {
 }
 </style>
 
-<!--<style>-->
-<!--.contact-sheet{-->
-<!--  padding-left: 10%;-->
-<!--  padding-right: 10%;-->
-<!--  padding-bottom: 10%;-->
-<!--}-->
 
-<!--* {box-sizing: border-box;}-->
-
-<!--.container {-->
-<!--  display: block;-->
-<!--  margin:auto;-->
-<!--  text-align: center;-->
-<!--  border-radius: 5px;-->
-<!--  background-color: #f2f2f2;-->
-<!--  padding: 20px;-->
-<!--  width: 50%;-->
-<!--}-->
-
-<!--label {-->
-<!--  float: left;-->
-<!--}-->
-
-<!--input[type=text], [type=email], textarea {-->
-<!--  width: 100%;-->
-<!--  padding: 12px;-->
-<!--  border: 1px solid #ccc;-->
-<!--  border-radius: 4px;-->
-<!--  box-sizing: border-box;-->
-<!--  margin-top: 6px;-->
-<!--  margin-bottom: 16px;-->
-<!--  resize: vertical;-->
-<!--}-->
-
-<!--input[type=submit] {-->
-<!--  background-color: #4CAF50;-->
-<!--  color: white;-->
-<!--  padding: 12px 20px;-->
-<!--  border: none;-->
-<!--  border-radius: 4px;-->
-<!--  cursor: pointer;-->
-<!--}-->
-
-<!--input[type=submit]:hover {-->
-<!--  background-color: #45a049;-->
-<!--}-->
-<!--</style>-->
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,26 +2,13 @@
   <div class="about">
 
 
-    <button v-on:click="fetchData">Get Data</button>
-    <h1>{{ this.proj }}</h1>
+    <h1>Project Route</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Projects',
-  proj: '',
-  url: 'https://spreadsheets.google.com/feeds/list/1UUsLTR3KfDVDztVxiJ9pC61R8jDDpiVVilF-fxHzp4U/od6/public/values?alt=json',
-  fetchData: function(){
-    fetch(this.url,{
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then(data => this.proj = data)
-  },
-
+  proj: ''
 }
 </script>
