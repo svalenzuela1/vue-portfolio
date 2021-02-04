bcd615163442a947f790c537d55de7cc22612015
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-03 21:56:47 -0500

    changing template

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,18 +1,18 @@
 <template>
-  <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button v-on:click="handleSubmit" type="submit">Send</button>
-    </p>
+  <form
+      ...
+      @submit.prevent="handleSubmit">
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+          type="radio"
+          name="panelist"
+          @input="ev => form.askPerson = ev.target.value"
+          :value="panelist"
+          :checked="form.askPerson === panelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
   </form>
 </template>
 <script>
